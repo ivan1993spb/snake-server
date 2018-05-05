d52656bf090c0f700ebe71bb496c61d3f8c1ef22
Ivan Pushkin <pushkin13@bk.ru>
2018-05-05 14:58:04 +0300

    Implement DeleteGameHandler

--- handlers/delete_game_handler.go
+++ handlers/delete_game_handler.go
@@ -1,29 +1,83 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
+
+	"github.com/ivan1993spb/snake-server/connections"
 )
 
-const URLRouteDeleteGame = "/game"
+const URLRouteDeleteGameByID = "/game/{id}"
 
 const MethodDeleteGame = http.MethodDelete
 
+const routeVarGroupID = "id"
+
+type responseDeleteGameHandler struct {
+	ID int `json:"id"`
+}
+
 type deleteGameHandler struct {
-	logger *logrus.Logger
+	logger       *logrus.Logger
+	groupManager *connections.ConnectionGroupManager
 }
 
 type ErrDeleteGameHandler string
 
-func NewDeleteGameHandler(logger *logrus.Logger) http.Handler {
-	return &createGameHandler{
-		logger: logger,
+func (e ErrDeleteGameHandler) Error() string {
+	return "delete game handler error: " + string(e)
+}
+
+func NewDeleteGameHandler(logger *logrus.Logger, groupManager *connections.ConnectionGroupManager) http.Handler {
+	return &deleteGameHandler{
+		logger:       logger,
+		groupManager: groupManager,
 	}
 }
 
 func (h *deleteGameHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	h.logger.Info("game handler start")
+	h.logger.Info("delete game handler start")
+	defer h.logger.Info("delete game handler end")
 
-	h.logger.Info("game handler end")
+	vars := mux.Vars(r)
+
+	id, err := strconv.Atoi(vars[routeVarGroupID])
+	if err != nil {
+		h.logger.Error(ErrDeleteGameHandler(err.Error()))
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+
+	h.logger.Infoln("group id to delete:", id)
+
+	if err := h.groupManager.Delete(id); err != nil {
+		h.logger.Error(ErrDeleteGameHandler(err.Error()))
+
+		switch err {
+		case connections.ErrDeleteNotFoundGroup:
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		case connections.ErrDeleteNotEmptyGroup:
+			http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+		default:
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	h.logger.Infoln("group deleted:", id)
+
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+
+	err = json.NewEncoder(w).Encode(responseDeleteGameHandler{
+		ID: id,
+	})
+	if err != nil {
+		h.logger.Error(ErrDeleteGameHandler(err.Error()))
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
 }
