076f8f379a6e0243f2572736e584afa3e7b5e8e7
Ivan Pushkin <pushkin13@bk.ru>
2018-05-05 20:11:39 +0300

    Rewrite events

--- game/event.go
+++ game/event.go
@@ -1,38 +1,15 @@
-// Copyright 2015 Pushkin Ivan. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package game
 
-import "encoding/json"
-
-type eventType uint8
+type EventType uint8
 
 const (
-	EVENT_TYPE_ERROR = iota
-	EVENT_TYPE_OBJECT_CREATING
-	EVENT_TYPE_OBJECT_DELETING
-	EVENT_TYPE_OBJECT_UPDATING
+	EventTypeError EventType = iota
+	EventTypeObjectCreate
+	EventTypeObjectDelete
+	EventTypeObjectUpdate
 )
 
-// Implementing json.Marshaler interface
-func (et eventType) MarshalJSON() ([]byte, error) {
-	switch et {
-	case EVENT_TYPE_ERROR:
-		return []byte(`"error"`), nil
-	case EVENT_TYPE_OBJECT_CREATING:
-		return []byte(`"creating"`), nil
-	case EVENT_TYPE_OBJECT_DELETING:
-		return []byte(`"deleting"`), nil
-	case EVENT_TYPE_OBJECT_UPDATING:
-		return []byte(`"updating"`), nil
-	}
-
-	// Don't return error for undefined event type
-	return []byte(`"undefined"`), nil
-}
-
-type event struct {
-	Type eventType      `json:"type"`
-	Data json.Marshaler `json:"data"`
+type Event struct {
+	Type    EventType
+	Payload interface{}
 }
