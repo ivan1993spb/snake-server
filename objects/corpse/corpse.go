2f7ef0d103af6112a0730381517307f12596d563
Ivan Pushkin <pushkin13@bk.ru>
2018-05-06 03:08:21 +0300

    Refactor object corpse

--- objects/corpse/corpse.go
+++ objects/corpse/corpse.go
@@ -4,10 +4,8 @@ import (
 	"errors"
 	"time"
 
-	"github.com/olebedev/emitter"
-
 	"github.com/ivan1993spb/snake-server/engine"
-	"github.com/ivan1993spb/snake-server/playground"
+	"github.com/ivan1993spb/snake-server/game"
 )
 
 // Time for which corpse will be lie on playground
@@ -15,51 +13,40 @@ const corpseMaxExperience = time.Second * 15
 
 // Snakes can eat corpses
 type Corpse struct {
-	pg          *playground.Playground
+	world       game.WorldInterface
 	location    engine.Location
 	nippedPiece *engine.Dot // last nipped piece
+	stop        chan struct{}
 }
 
 // Corpses are created when a snake dies
-func CreateCorpse(pg *playground.Playground, location engine.Location) (*Corpse, error) {
+func CreateCorpse(world game.WorldInterface, location engine.Location) (*Corpse, error) {
 	// TODO: Check location
 
 	corpse := &Corpse{}
 
-	if location, _ := pg.CreateObjectAvailableDots(corpse, location); len(location) > 0 {
-		return &Corpse{
-			pg:          pg,
-			location:    location,
-			nippedPiece: nil,
-		}, nil
+	if location, _ := world.CreateObjectAvailableDots(corpse, location); len(location) > 0 {
+		corpse.world = world
+		corpse.location = location
+		corpse.stop = make(chan struct{}, 0)
+		go corpse.run()
+		return corpse, nil
 	}
+
 	return nil, errors.New("")
 }
 
-// Implementing playground.Location interface
-func (c *Corpse) DotCount() uint16 {
-	return uint16(len(c.location))
-}
-
-// Implementing playground.Location interface
-func (c *Corpse) Dot(i uint16) *engine.Dot {
-	if c.DotCount() > i {
-		return c.location[i]
-	}
-	return nil
-}
-
-// Implementing logic.Food interface
 func (c *Corpse) NutritionalValue(dot *engine.Dot) int8 {
 	if c.location.Contains(dot) {
 		newDots := c.location.Delete(dot)
 
 		if len(c.location) > 0 {
-			c.pg.UpdateObjectAvailableDots(c, c.location, newDots)
+			c.world.UpdateObjectAvailableDots(c, c.location, newDots)
 			c.location = newDots
 			c.nippedPiece = dot
 		} else {
-			c.pg.DeleteObject(c, c.location)
+			c.world.DeleteObject(c, c.location)
+			close(c.stop)
 		}
 
 		return 2
@@ -68,8 +55,13 @@ func (c *Corpse) NutritionalValue(dot *engine.Dot) int8 {
 	return 0
 }
 
-func (c *Corpse) Run(emitter *emitter.Emitter) {
-	go func() {
-
-	}()
+func (c *Corpse) run() {
+	var timer = time.NewTimer(corpseMaxExperience)
+	defer timer.Stop()
+	select {
+	case <-timer.C:
+		c.world.DeleteObject(c, c.location)
+		close(c.stop)
+	case <-c.stop:
+	}
 }
