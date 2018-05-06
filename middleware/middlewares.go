e85b11f3482e62d4ffdf6bbbbae9be3ac09b2696
Ivan Pushkin <pushkin13@bk.ru>
2018-05-06 08:21:02 +0300

    Create middlewares

---
+++ middleware/middlewares.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/negroni"
+)
+
+type textPanicFormatter struct{}
+
+func (t *textPanicFormatter) FormatPanicError(rw http.ResponseWriter, r *http.Request, infos *negroni.PanicInformation) {
+	if rw.Header().Get("Content-Type") == "" {
+		rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	}
+	fmt.Fprintf(rw, http.StatusText(http.StatusInternalServerError))
+}
+
+func NewRecovery(logger *logrus.Logger) negroni.Handler {
+	middleware := negroni.NewRecovery()
+	middleware.PrintStack = false
+	middleware.Logger = logger
+	middleware.Formatter = &textPanicFormatter{}
+	return middleware
+}
+
+const httpLoggerFormat = "{{.StartTime}} | {{.Status}} | {{.Duration}} | {{.Method}} {{.Path}} \n"
+
+func NewLogger(logger *logrus.Logger) negroni.Handler {
+	middleware := negroni.NewLogger()
+	middleware.SetFormat(httpLoggerFormat)
+	middleware.ALogger = logger
+	return middleware
+}
