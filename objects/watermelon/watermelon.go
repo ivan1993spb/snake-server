c1ae42481a32001d8aa4c4028866e982b07aaca0
Ivan Pushkin <pushkin13@bk.ru>
2018-05-06 03:09:51 +0300

    Refactor watermelon object

--- objects/watermelon/watermelon.go
+++ objects/watermelon/watermelon.go
@@ -1,17 +1,11 @@
-// Copyright 2015 Pushkin Ivan. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package watermelon
 
 import (
 	"math/rand"
 	"time"
 
-	"github.com/olebedev/emitter"
-
 	"github.com/ivan1993spb/snake-server/engine"
-	"github.com/ivan1993spb/snake-server/playground"
+	"github.com/ivan1993spb/snake-server/game"
 )
 
 const (
@@ -29,67 +23,35 @@ const (
 )
 
 type Watermelon struct {
-	pg       *playground.Playground
+	world    game.WorldInterface
 	location engine.Location
 }
 
-func CreateWatermelon(pg *playground.Playground) (*Watermelon, error) {
+func CreateWatermelon(world game.WorldInterface) (*Watermelon, error) {
 
 	watermelon := &Watermelon{}
 
-	location, err := pg.CreateObjectRandomRect(watermelon, watermelonWidth, watermelonHeight)
+	location, err := world.CreateObjectRandomRect(watermelon, watermelonWidth, watermelonHeight)
 	if err != nil {
 		// TODO: Create specific error.
 		return nil, err
 	}
 
-	watermelon.pg = pg
+	watermelon.world = world
 	watermelon.location = location
 
 	return watermelon, nil
 }
 
-// Implementing playground.Location interface
-func (w *Watermelon) DotCount() (c uint16) {
-	for _, dot := range w.location {
-		if dot != nil {
-			c++
-		}
-	}
-
-	return
-}
-
-// Implementing playground.Location interface
-func (w *Watermelon) Dot(i uint16) (dot *engine.Dot) {
-	if i < w.DotCount() {
-		var j uint16
-		for _, dot = range w.location {
-			if dot != nil {
-				if i == j {
-					break
-				}
-				j++
-			}
-		}
-	}
-	return
-}
-
-// Implementing logic.Food interface
 func (w *Watermelon) NutritionalValue(dot *engine.Dot) int8 {
 	for _, dot := range w.location {
 		if dot.Equals(dot) {
 			location := w.location.Delete(dot)
-			w.pg.UpdateObject(w, w.location, location)
+			w.world.UpdateObject(w, w.location, location)
 			w.location = location
 			return watermelonMinNutrValue + int8(rand.Intn(watermelonNutrVar))
 		}
 	}
 
 	return 0
-}
-
-func (w *Watermelon) Run(emitter *emitter.Emitter) error {
-	// TODO: Implement method.
 }
