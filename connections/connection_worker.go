45dd39f186b35e3a9500bd83e2e6cec6777095fa
Ivan Pushkin <pushkin13@bk.ru>
2018-05-09 11:56:01 +0300

    Create some dumbs methods in ConnectionWorker for reading and writing in ws and listening game events

--- connections/connection_worker.go
+++ connections/connection_worker.go
@@ -1,26 +1,108 @@
 package connections
 
 import (
-	"time"
-
 	"github.com/gorilla/websocket"
 	"github.com/sirupsen/logrus"
 
 	"github.com/ivan1993spb/snake-server/game"
+	"github.com/ivan1993spb/snake-server/objects/snake"
 )
 
 type ConnectionWorker struct {
-	conn   *websocket.Conn
-	logger *logrus.Logger
+	conn      *websocket.Conn
+	logger    *logrus.Logger
+	chStop    chan struct{}
+	chStopErr chan error
+	chOutput  chan OutputMessage
+	chInput   chan InputMessageType
+
+	flagStarted bool
+	flagStopped bool
 }
 
 func NewConnectionWorker(conn *websocket.Conn, logger *logrus.Logger) *ConnectionWorker {
 	return &ConnectionWorker{
-		conn:   conn,
-		logger: logger,
+		conn:      conn,
+		logger:    logger,
+		chStopErr: make(chan error, 0),
 	}
 }
 
-func (c *ConnectionWorker) Run(game *game.Game) error {
-	return c.conn.WriteControl(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, "close"), time.Now().Add(time.Second))
+func (cw *ConnectionWorker) Start(game *game.Game) error {
+	//return cw.conn.WriteControl(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, "close"), time.Now().Add(time.Second))
+
+	if cw.flagStarted {
+		// Return error
+		return nil
+	}
+
+	cw.startRead()
+	cw.startWrite()
+
+	// Start connection read
+	// Start connection write
+	// Listen game events
+	// Parse input messages (?) and send game commands
+
+	s, _ := snake.CreateSnake(game.World())
+	go game.RunObserver(s, 16)
+	//game.RunObserver(cw, 16)
+
+	cw.flagStarted = true
+
+	return <-cw.chStopErr
+}
+
+func (cw *ConnectionWorker) startRead() {
+	go func() {
+		for {
+			//messageType, r, err := cw.conn.NextReader()
+			_, _, err := cw.conn.NextReader()
+			if err != nil {
+				cw.chStopErr <- err
+				return
+			}
+		}
+	}()
+}
+
+func (cw *ConnectionWorker) startWrite() {
+	go func() {
+		for {
+			select {
+			case message := <-cw.chOutput:
+				cw.logger.Info(message)
+				//err := cw.writeMessage(message)
+				// TODO: Handler error.
+			case <-cw.chStop:
+				return
+			}
+
+		}
+	}()
+}
+
+func (cw *ConnectionWorker) writeMessage(message *OutputMessage) error {
+	w, err := cw.conn.NextWriter(websocket.TextMessage)
+	if err != nil {
+		return nil
+	}
+
+	// TODO: Write message.
+
+	// TODO: Return error.
+
+	w.Close()
+	return nil
+}
+
+func (cw *ConnectionWorker) Run(ch <-chan game.Event) {
+	for {
+		select {
+		case event := <-ch:
+			cw.logger.Info(event)
+		case <-cw.chStop:
+			return
+		}
+	}
 }
