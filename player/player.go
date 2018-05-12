cabaa4999aa686612a5978eadac199c938b5a629
Ivan Pushkin <pushkin13@bk.ru>
2018-05-12 19:32:08 +0300

    Create initializing snale in player

--- player/player.go
+++ player/player.go
@@ -1,3 +1,22 @@
 package player
 
-type Player struct{}
+import (
+	"github.com/ivan1993spb/snake-server/game"
+	"github.com/ivan1993spb/snake-server/objects/snake"
+)
+
+type Player struct {
+	game *game.Game
+}
+
+func NewPlayer(game *game.Game) *Player {
+	return &Player{
+		game: game,
+	}
+}
+
+func (p *Player) Start() {
+	s, _ := snake.CreateSnake(p.game.World())
+	// TODO: Pass stop channel?
+	s.Run(p.game.Events(make(chan struct{})))
+}
