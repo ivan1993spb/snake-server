c9f22a6baed84ae48fc91a326f4a0fc4b4a1b596
Ivan Pushkin <pushkin13@bk.ru>
2018-05-13 21:36:38 +0300

    Implement stringer interface for game.EventType

--- game/event.go
+++ game/event.go
@@ -10,6 +10,21 @@ const (
 	EventTypeObjectChecked
 )
 
+var eventsLabels = map[EventType]string{
+	EventTypeError:         "error",
+	EventTypeObjectCreate:  "create",
+	EventTypeObjectDelete:  "delete",
+	EventTypeObjectUpdate:  "update",
+	EventTypeObjectChecked: "checked",
+}
+
+func (event EventType) String() string {
+	if label, ok := eventsLabels[event]; ok {
+		return label
+	}
+	return "unknown"
+}
+
 type Event struct {
 	Type    EventType
 	Payload interface{}
