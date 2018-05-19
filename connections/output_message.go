43a0689d95260f904090c4ca57056967827f455f
Ivan Pushkin <pushkin13@bk.ru>
2018-05-19 09:22:17 +0300

    Create stringer method for OutputMessage struct

--- connections/output_message.go
+++ connections/output_message.go
@@ -4,8 +4,22 @@ type OutputMessageType uint8
 
 const (
 	OutputMessageTypeGameEvent OutputMessageType = iota
-	OutputMessageGroupNotice
+	OutputMessageTypeGroupNotice
+	OutputMessageTypeConnectionNotice
 )
+
+var outputMessageTypeLabels = map[OutputMessageType]string{
+	OutputMessageTypeGameEvent:        "game_event",
+	OutputMessageTypeGroupNotice:      "group_notice",
+	OutputMessageTypeConnectionNotice: "connection_notice",
+}
+
+func (t OutputMessageType) String() string {
+	if label, ok := outputMessageTypeLabels[t]; ok {
+		return label
+	}
+	return "unknown"
+}
 
 type OutputMessage struct {
 	Type    OutputMessageType
