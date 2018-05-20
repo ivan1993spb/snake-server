e1dabfe7d46656e8f98bfef9f218d61440e04687
Ivan Pushkin <pushkin13@bk.ru>
2018-05-20 16:14:51 +0300

    Create tests for scene LocateAvailableDots method

--- engine/scene_test.go
+++ engine/scene_test.go
@@ -45,3 +45,58 @@ func Test_Scene_LocateRandomRect(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, []Location{location}, scene.locations)
 }
+
+func Test_Scene_LocateAvailableDots_EmptyScene(t *testing.T) {
+	scene := &Scene{
+		area: Area{
+			width:  100,
+			height: 100,
+		},
+		locations:      []Location{},
+		locationsMutex: &sync.RWMutex{},
+	}
+
+	location := Location{Dot{1, 1}, Dot{1, 2}}
+	locationActual := scene.LocateAvailableDots(location)
+	require.Equal(t, []Location{location}, scene.locations)
+	require.Equal(t, []Location{locationActual}, scene.locations)
+	require.Equal(t, location, locationActual)
+}
+
+func Test_Scene_LocateAvailableDots_LocationNotAvailable(t *testing.T) {
+	scene := &Scene{
+		area: Area{
+			width:  100,
+			height: 100,
+		},
+		locations: []Location{
+			{Dot{1, 1}, Dot{1, 2}},
+		},
+		locationsMutex: &sync.RWMutex{},
+	}
+
+	location := Location{Dot{1, 1}, Dot{1, 2}}
+	locationActual := scene.LocateAvailableDots(location)
+	require.Equal(t, Location{}, locationActual)
+}
+
+func Test_Scene_LocateAvailableDots_LocationsIntersects(t *testing.T) {
+	scene := &Scene{
+		area: Area{
+			width:  100,
+			height: 100,
+		},
+		locations: []Location{
+			{Dot{1, 1}, Dot{1, 2}, Dot{1, 3}, Dot{1, 4}},
+		},
+		locationsMutex: &sync.RWMutex{},
+	}
+
+	location := Location{Dot{1, 1}, Dot{1, 0}}
+	locationActual := scene.LocateAvailableDots(location)
+	require.Equal(t, Location{Dot{1, 0}}, locationActual)
+	require.Equal(t, []Location{
+		{Dot{1, 1}, Dot{1, 2}, Dot{1, 3}, Dot{1, 4}},
+		{Dot{1, 0}},
+	}, scene.locations)
+}
