022f318a33b8c1e03ba96da490070d6f29f548bb
Ivan Pushkin <pushkin13@bk.ru>
2018-05-20 17:06:15 +0300

    Create TODO in logger middleware

--- middlewares/logger.go
+++ middlewares/logger.go
@@ -7,6 +7,8 @@ import (
 
 const httpLoggerFormat = "request processed: {{.StartTime}} | {{.Status}} | {{.Duration}} | {{.Method}} {{.Path}}"
 
+// TODO: Use https://github.com/meatballhat/negroni-logrus ?
+
 func NewLogger(logger logrus.FieldLogger) negroni.Handler {
 	middleware := negroni.NewLogger()
 	middleware.SetFormat(httpLoggerFormat)
