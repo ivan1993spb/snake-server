e4e42ed56a18afcb3bc780d0ccd1612485c82a9d
Ivan Pushkin <iv.pushk@gmail.com>
2018-06-02 14:32:25 +0300

    Create get capacity handler

---
+++ handlers/get_capacity_handler.go
@@ -0,0 +1,49 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/ivan1993spb/snake-server/connections"
+)
+
+const URLRouteGetCapacity = "/capacity"
+
+const MethodGetCapacity = http.MethodGet
+
+type responseGetCapacityHandler struct {
+	Capacity float32 `json:"capacity"`
+}
+
+type getCapacityHandler struct {
+	logger       logrus.FieldLogger
+	groupManager *connections.ConnectionGroupManager
+}
+
+type ErrGetCapacityHandler string
+
+func (e ErrGetCapacityHandler) Error() string {
+	return "get capacity handler error: " + string(e)
+}
+
+func NewGetCapacityHandler(logger logrus.FieldLogger, groupManager *connections.ConnectionGroupManager) http.Handler {
+	return &getCapacityHandler{
+		logger:       logger,
+		groupManager: groupManager,
+	}
+}
+
+func (h *getCapacityHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	err := json.NewEncoder(w).Encode(responseGetCapacityHandler{
+		Capacity: h.groupManager.Capacity(),
+	})
+	if err != nil {
+		h.logger.Error(ErrGetGameHandler(err.Error()))
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+}
