845c8ed154969d70730b793c524cb63e203d3bf7
Ivan Pushkin <iv.pushk@gmail.com>
2018-06-02 14:37:43 +0300

    Rewrite corpse

--- objects/corpse/corpse.go
+++ objects/corpse/corpse.go
@@ -3,6 +3,7 @@ package corpse
 import (
 	"errors"
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/pquerna/ffjson/ffjson"
@@ -14,67 +15,94 @@ import (
 // Time for which corpse will be lie on playground
 const corpseMaxExperience = time.Second * 15
 
+const corpseNutritionalValue uint16 = 2
+
 // Snakes can eat corpses
 type Corpse struct {
 	world       *world.World
 	location    engine.Location
 	nippedPiece engine.Dot // last nipped piece
+	mux         *sync.RWMutex
 	stop        chan struct{}
 }
 
 // Corpse are created when a snake dies
 func NewCorpse(world *world.World, location engine.Location) (*Corpse, error) {
-	// TODO: Check location.
-
-	// TODO: Pass stopper.
-
-	corpse := &Corpse{}
-
-	if location, _ := world.CreateObjectAvailableDots(corpse, location); len(location) > 0 {
-		corpse.world = world
-		corpse.location = location
-		corpse.stop = make(chan struct{}, 0)
-		go corpse.run()
-		return corpse, nil
+	if location.Empty() {
+		return nil, errors.New("location is empty")
 	}
 
-	return nil, errors.New("")
+	corpse := &Corpse{
+		mux: &sync.RWMutex{},
+	}
+
+	location, err := world.CreateObjectAvailableDots(corpse, location)
+	if err != nil {
+		// TODO:
+		fmt.Println("create", err)
+		return nil, err
+	}
+
+	if len(location) == 0 {
+		return nil, errors.New("no location available")
+	}
+
+	corpse.mux.Lock()
+	corpse.world = world
+	corpse.location = location
+	corpse.stop = make(chan struct{})
+	corpse.mux.Unlock()
+
+	return corpse, nil
 }
 
 func (c *Corpse) String() string {
+	c.mux.RLock()
+	defer c.mux.RUnlock()
 	return fmt.Sprint("corpse ", c.location)
 }
 
 func (c *Corpse) NutritionalValue(dot engine.Dot) uint16 {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+
 	if c.location.Contains(dot) {
 		newDots := c.location.Delete(dot)
 
-		if len(c.location) > 0 {
-			c.world.UpdateObjectAvailableDots(c, c.location, newDots)
-			c.location = newDots
+		if len(newDots) > 0 {
+			// TODO: Handle errors.
+			newLoc, _ := c.world.UpdateObjectAvailableDots(c, c.location, newDots)
+			c.location = newLoc
 			c.nippedPiece = dot
 		} else {
 			c.world.DeleteObject(c, c.location)
 			close(c.stop)
 		}
 
-		return 2
+		return corpseNutritionalValue
 	}
 
 	return 0
 }
 
-func (c *Corpse) run() {
+func (c *Corpse) Run(stop <-chan struct{}) {
 	var timer = time.NewTimer(corpseMaxExperience)
 	defer timer.Stop()
 	select {
+	case <-stop:
+		// global stop
 	case <-timer.C:
+		c.mux.RLock()
 		c.world.DeleteObject(c, c.location)
+		c.mux.RUnlock()
 		close(c.stop)
 	case <-c.stop:
+		// Corpse was eaten.
 	}
 }
 
 func (c *Corpse) MarshalJSON() ([]byte, error) {
+	c.mux.RLock()
+	defer c.mux.RUnlock()
 	return ffjson.Marshal(c.location)
 }
---
+++ objects/corpse/corpse_test.go
@@ -0,0 +1,102 @@
+package corpse
+
+import (
+	"sync"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/ivan1993spb/snake-server/engine"
+	"github.com/ivan1993spb/snake-server/world"
+)
+
+func Test_NewCorpse_CreatesCorpseAndLocatesObject(t *testing.T) {
+	w, err := world.NewWorld(100, 100)
+	require.Nil(t, err, "cannot initialize world")
+	require.NotNil(t, w, "cannot initialize world")
+
+	corpse, err := NewCorpse(w, engine.Location{
+		engine.Dot{10, 0},
+		engine.Dot{9, 0},
+		engine.Dot{8, 0},
+		engine.Dot{7, 0},
+	})
+	require.Nil(t, err)
+	require.Equal(t, engine.Location{
+		engine.Dot{10, 0},
+		engine.Dot{9, 0},
+		engine.Dot{8, 0},
+		engine.Dot{7, 0},
+	}, corpse.location)
+}
+
+func Test_Corpse_NutritionalValue_ReturnsValidNutritionalValue(t *testing.T) {
+	w, err := world.NewWorld(100, 100)
+	require.Nil(t, err, "cannot initialize world")
+	require.NotNil(t, w, "cannot initialize world")
+
+	corpse := &Corpse{
+		world: w,
+		location: engine.Location{
+			engine.Dot{10, 0},
+			engine.Dot{9, 0},
+			engine.Dot{8, 0},
+			engine.Dot{7, 0},
+		},
+		nippedPiece: engine.Dot{},
+		mux:         &sync.RWMutex{},
+		stop:        make(chan struct{}),
+	}
+
+	err = w.CreateObject(corpse, engine.Location{
+		engine.Dot{10, 0},
+		engine.Dot{9, 0},
+		engine.Dot{8, 0},
+		engine.Dot{7, 0},
+	})
+	require.Nil(t, err, "cannot create object")
+
+	nutritionalValue := corpse.NutritionalValue(engine.Dot{10, 0})
+	require.Equal(t, corpseNutritionalValue, nutritionalValue)
+	require.Equal(t, engine.Location{
+		engine.Dot{9, 0},
+		engine.Dot{8, 0},
+		engine.Dot{7, 0},
+	}, corpse.location)
+}
+
+func Test_Corpse_NutritionalValue_ReturnsZeroForInvalidDot(t *testing.T) {
+	w, err := world.NewWorld(100, 100)
+	require.Nil(t, err, "cannot initialize world")
+	require.NotNil(t, w, "cannot initialize world")
+
+	corpse := &Corpse{
+		world: w,
+		location: engine.Location{
+			engine.Dot{10, 0},
+			engine.Dot{9, 0},
+			engine.Dot{8, 0},
+			engine.Dot{7, 0},
+		},
+		nippedPiece: engine.Dot{1, 2},
+		mux:         &sync.RWMutex{},
+		stop:        make(chan struct{}),
+	}
+
+	err = w.CreateObject(corpse, engine.Location{
+		engine.Dot{10, 0},
+		engine.Dot{9, 0},
+		engine.Dot{8, 0},
+		engine.Dot{7, 0},
+	})
+	require.Nil(t, err, "cannot create object")
+
+	nutritionalValue := corpse.NutritionalValue(engine.Dot{10, 10})
+	require.Equal(t, uint16(0), nutritionalValue)
+	require.Equal(t, engine.Location{
+		engine.Dot{10, 0},
+		engine.Dot{9, 0},
+		engine.Dot{8, 0},
+		engine.Dot{7, 0},
+	}, corpse.location)
+}
