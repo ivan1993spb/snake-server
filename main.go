d98a4dab9e0937dc683270f8867b8944b51480b7
Ivan Pushkin <iv.pushk@gmail.com>
2018-06-02 14:39:31 +0300

    Create routes handlers in main.go

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"math/rand"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -15,10 +17,17 @@ import (
 	"github.com/ivan1993spb/snake-server/middlewares"
 )
 
+const ServerName = "Snake-Server"
+
 const (
 	defaultAddress     = ":8080"
 	defaultGroupsLimit = 100
 	defaultConnsLimit  = 1000
+)
+
+var (
+	Version = "dev"
+	Build   = "dev"
 )
 
 var (
@@ -30,6 +39,13 @@ var (
 	logLevel    string
 )
 
+func usage() {
+	fmt.Fprint(os.Stderr, "Wellcome to snake-server!\n\n")
+	fmt.Fprintf(os.Stderr, "Server version %s, build %s\n\n", Version, Build)
+	fmt.Fprintf(os.Stderr, "Usage: %s [options]\n\n", os.Args[0])
+	flag.PrintDefaults()
+}
+
 func init() {
 	flag.StringVar(&address, "address", defaultAddress, "address to serve")
 	flag.IntVar(&groupsLimit, "groups-limit", defaultGroupsLimit, "groups limit")
@@ -37,6 +53,7 @@ func init() {
 	flag.Int64Var(&seed, "seed", time.Now().UnixNano(), "random seed")
 	flag.BoolVar(&flagJSONLog, "log-json", false, "use json format for logger")
 	flag.StringVar(&logLevel, "log-level", "info", "set log level: panic, fatal, error, warning (warn), info or debug")
+	flag.Usage = usage
 	flag.Parse()
 }
 
@@ -44,6 +61,9 @@ func logger() *logrus.Logger {
 	logger := logrus.New()
 	if flagJSONLog {
 		logger.Formatter = &logrus.JSONFormatter{}
+	} else {
+		// TODO: Set up formatter with colors for windows ?
+		// https://github.com/sirupsen/logrus/issues/172
 	}
 	if level, err := logrus.ParseLevel(logLevel); err != nil {
 		logger.SetLevel(logrus.InfoLevel)
@@ -55,6 +75,11 @@ func logger() *logrus.Logger {
 
 func main() {
 	logger := logger()
+
+	logger.WithFields(logrus.Fields{
+		"version": Version,
+		"build":   Build,
+	}).Info("wellcome to snake server!")
 
 	logger.WithFields(logrus.Fields{
 		"conns_limit":  connsLimit,
@@ -77,6 +102,8 @@ func main() {
 
 	// API routes
 	apiRouter := mux.NewRouter().StrictSlash(true)
+	apiRouter.Path(handlers.URLRouteGetInfo).Methods(handlers.MethodGetInfo).Handler(handlers.NewGetInfoHandler(logger))
+	apiRouter.Path(handlers.URLRouteGetCapacity).Methods(handlers.MethodGetCapacity).Handler(handlers.NewGetCapacityHandler(logger, groupManager))
 	apiRouter.Path(handlers.URLRouteCreateGame).Methods(handlers.MethodCreateGame).Handler(handlers.NewCreateGameHandler(logger, groupManager))
 	apiRouter.Path(handlers.URLRouteGetGameByID).Methods(handlers.MethodGetGame).Handler(handlers.NewGetGameHandler(logger, groupManager))
 	apiRouter.Path(handlers.URLRouteDeleteGameByID).Methods(handlers.MethodDeleteGame).Handler(handlers.NewDeleteGameHandler(logger, groupManager))
@@ -90,6 +117,7 @@ func main() {
 	))
 
 	n := negroni.New()
+	n.Use(middlewares.NewServerInfo(ServerName, Version, Build))
 	n.UseHandler(rootRouter)
 
 	logger.WithField("address", address).Info("starting server")
