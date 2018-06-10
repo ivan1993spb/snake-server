e91fa495631ff75320600c47c8a2f19614038247
Ivan Pushkin <pushkin13@bk.ru>
2018-06-10 10:59:41 +0300

    Fix apple observer to create apple in starting goroutine

--- observers/apple_observer.go
+++ observers/apple_observer.go
@@ -16,22 +16,22 @@ const oneAppleArea = 50
 type AppleObserver struct{}
 
 func (AppleObserver) Observe(stop <-chan struct{}, w *world.World, logger logrus.FieldLogger) {
-	appleCount := defaultAppleCount
-	size := w.Size()
-
-	if size > oneAppleArea {
-		appleCount = int(size / oneAppleArea)
-	}
-
-	logger.Debugf("apple count for size %d = %d", size, appleCount)
-
-	for i := 0; i < appleCount; i++ {
-		if _, err := apple.NewApple(w); err != nil {
-			logger.WithError(err).Error("cannot create apple")
-		}
-	}
-
 	go func() {
+		appleCount := defaultAppleCount
+		size := w.Size()
+
+		if size > oneAppleArea {
+			appleCount = int(size / oneAppleArea)
+		}
+
+		logger.Debugf("apple count for size %d = %d", size, appleCount)
+
+		for i := 0; i < appleCount; i++ {
+			if _, err := apple.NewApple(w); err != nil {
+				logger.WithError(err).Error("cannot create apple")
+			}
+		}
+
 		for event := range w.Events(stop, chanAppleObserverEventsBuffer) {
 			if event.Type == world.EventTypeObjectDelete {
 				if _, ok := event.Payload.(*apple.Apple); ok {
