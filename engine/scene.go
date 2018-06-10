86d546a3cad648962e226549fc7b71b69edb81b3
Ivan Pushkin <pushkin13@bk.ru>
2018-06-10 09:52:30 +0300

    Create method LocateRandomRectMargin for engine.Scene and test

--- engine/scene.go
+++ engine/scene.go
@@ -370,19 +370,58 @@ func (s *Scene) LocateRandomRect(rw, rh uint8) (Location, error) {
 	return s.unsafeLocateRandomRect(rw, rh)
 }
 
+func (s *Scene) unsafeLocateRandomRectMarginTryOnce(rw, rh, margin uint8) (Location, error) {
+	if rect, err := s.area.NewRandomRect(rw+margin*2, rh+margin*2, 0, 0); err == nil {
+		for i := uint16(0); i < rect.DotCount(); i++ {
+			dot := rect.Dot(i)
+
+			if !s.area.Contains(dot) {
+				return nil, errors.New("area not contains generated dot")
+			}
+
+			for i := range s.locations {
+				if s.locations[i].Contains(dot) {
+					return nil, errors.New("generated dot is occupied")
+				}
+			}
+		}
+
+		resultRect := &Rect{
+			x: rect.x + margin,
+			y: rect.y + margin,
+			w: rect.w - margin*2,
+			h: rect.h - margin*2,
+		}
+
+		return resultRect.Location(), nil
+	} else {
+		return nil, err
+	}
+}
+
 func (s *Scene) unsafeLocateRandomRectMargin(rw, rh, margin uint8) (Location, error) {
-	// TODO: Implement method.
-	return Location{}, nil
+	if margin == 0 {
+		return s.unsafeLocateRandomRect(rw, rh)
+	}
+
+	for count := 0; count < FindRetriesNumber; count++ {
+		if rect, err := s.unsafeLocateRandomRectMarginTryOnce(rw, rh, margin); err == nil {
+			return rect, nil
+		}
+	}
+
+	return nil, ErrRetriesLimit
 }
 
 func (s *Scene) LocateRandomRectMargin(rw, rh, margin uint8) (Location, error) {
+	s.locationsMutex.Lock()
+	defer s.locationsMutex.Unlock()
+
 	if margin == 0 {
-		// TODO: Handle this case.
+		return s.unsafeLocateRandomRect(rw, rh)
 	}
 
-	// TODO: Implement method.
-
-	return Location{}, nil
+	return s.unsafeLocateRandomRectMargin(rw, rh, margin)
 }
 
 func (s *Scene) Navigate(dot Dot, dir Direction, dis uint8) (Dot, error) {
