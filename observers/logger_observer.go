018d12868144c7756d4d2178de27b4eb4ce8dac0
Ivan Pushkin <pushkin13@bk.ru>
2018-06-10 11:24:10 +0300

    Create Logger game observer

---
+++ observers/logger_observer.go
@@ -0,0 +1,24 @@
+package observers
+
+import (
+	"github.com/sirupsen/logrus"
+
+	"github.com/ivan1993spb/snake-server/world"
+)
+
+type LoggerObserver struct{}
+
+func (LoggerObserver) Observe(stop <-chan struct{}, w *world.World, logger logrus.FieldLogger) {
+	go func() {
+		for event := range w.Events(stop, chanSnakeObserverEventsBuffer) {
+			switch event.Type {
+			case world.EventTypeError:
+				if err, ok := event.Payload.(error); ok {
+					logger.WithError(err).Error("world error")
+				}
+			case world.EventTypeObjectCreate, world.EventTypeObjectDelete, world.EventTypeObjectUpdate, world.EventTypeObjectChecked:
+				logger.WithField("payload", event.Payload).Debug("world event")
+			}
+		}
+	}()
+}
