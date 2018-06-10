ec6912dfd22e33365a6eb5f56d2a9c83ecdb5e29
Ivan Pushkin <pushkin13@bk.ru>
2018-06-10 13:10:13 +0300

    Refactor apple. Create valid apple json marshaling

--- objects/apple/apple.go
+++ objects/apple/apple.go
@@ -5,15 +5,17 @@ import (
 	"sync"
 
 	"github.com/pquerna/ffjson/ffjson"
+	"github.com/satori/go.uuid"
 
 	"github.com/ivan1993spb/snake-server/engine"
 	"github.com/ivan1993spb/snake-server/world"
 )
 
 type Apple struct {
-	world    *world.World
-	location engine.Location
-	mux      *sync.RWMutex
+	uuid  string
+	world *world.World
+	dot   engine.Dot
+	mux   *sync.RWMutex
 }
 
 type ErrCreateApple string
@@ -25,16 +27,20 @@ func (e ErrCreateApple) Error() string {
 // NewApple creates and locates new apple
 func NewApple(world *world.World) (*Apple, error) {
 	apple := &Apple{
-		mux: &sync.RWMutex{},
+		uuid: uuid.Must(uuid.NewV4()).String(),
+		mux:  &sync.RWMutex{},
 	}
 
 	location, err := world.CreateObjectRandomDot(apple)
 	if err != nil {
 		return nil, ErrCreateApple(err.Error())
 	}
+	if len(location) == 0 {
+		return nil, ErrCreateApple("created empty location")
+	}
 
 	apple.mux.Lock()
-	apple.location = location
+	apple.dot = location.Dot(0)
 	apple.world = world
 	apple.mux.Unlock()
 
@@ -44,16 +50,16 @@ func NewApple(world *world.World) (*Apple, error) {
 func (a *Apple) String() string {
 	a.mux.RLock()
 	defer a.mux.RUnlock()
-	return fmt.Sprintf("apple %s", a.location)
+	return fmt.Sprintf("apple %s", a.dot)
 }
 
 func (a *Apple) NutritionalValue(dot engine.Dot) uint16 {
 	a.mux.RLock()
 	defer a.mux.RUnlock()
 
-	if a.location.Equals(engine.Location{dot}) {
-		// TODO: Handle error.
-		a.world.DeleteObject(a, a.location)
+	if a.dot.Equals(dot) {
+		// TODO: Handle error?
+		a.world.DeleteObject(a, engine.Location{a.dot})
 		return 1
 	}
 
@@ -63,5 +69,15 @@ func (a *Apple) NutritionalValue(dot engine.Dot) uint16 {
 func (a *Apple) MarshalJSON() ([]byte, error) {
 	a.mux.RLock()
 	defer a.mux.RUnlock()
-	return ffjson.Marshal(a.location)
+	return ffjson.Marshal(&apple{
+		UUID: a.uuid,
+		Dot:  a.dot,
+		Type: "apple",
+	})
+}
+
+type apple struct {
+	UUID string     `json:"uuid"`
+	Dot  engine.Dot `json:"dot"`
+	Type string     `json:"type"`
 }
