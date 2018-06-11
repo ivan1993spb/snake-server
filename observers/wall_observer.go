1fc5a14564acf1c6d271a726860630b936da54fd
Ivan Pushkin <pushkin13@bk.ru>
2018-06-11 17:45:06 +0300

    Fix WallObserver to use wall.NewWallRuins wall generator

--- observers/wall_observer.go
+++ observers/wall_observer.go
@@ -7,16 +7,12 @@ import (
 	"github.com/ivan1993spb/snake-server/world"
 )
 
-const wallPerNDots = 100
-
 type WallObserver struct{}
 
 func (WallObserver) Observe(stop <-chan struct{}, w *world.World, logger logrus.FieldLogger) {
 	go func() {
-		for i := uint16(0); i < w.Size()/wallPerNDots; i++ {
-			if _, err := wall.NewRandWall(w); err != nil {
-				logger.WithError(err).Error("cannot create rand wall")
-			}
+		if _, err := wall.NewWallRuins(w); err != nil {
+			logger.WithError(err).Error("cannot create wall ruins")
 		}
 	}()
 }
