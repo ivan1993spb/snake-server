d0bbcb47934b5251572372e5b45488e451f82885
Ivan Pushkin <iv.pushk@gmail.com>
2018-06-16 16:57:16 +0300

    Create test case in dot tests

--- engine/dot_test.go
+++ engine/dot_test.go
@@ -9,9 +9,11 @@ import (
 func Test_Dot_Hash(t *testing.T) {
 	require.Equal(t, uint16(0xffaa), Dot{X: 0xff, Y: 0xaa}.Hash())
 	require.Equal(t, uint16(0xabcd), Dot{X: 0xab, Y: 0xcd}.Hash())
+	require.Equal(t, uint16(0x0), Dot{X: 0x0, Y: 0x0}.Hash())
 }
 
 func Test_HashToDot(t *testing.T) {
 	require.Equal(t, Dot{X: 0xff, Y: 0xaa}, HashToDot(0xffaa))
 	require.Equal(t, Dot{X: 0xab, Y: 0xcd}, HashToDot(0xabcd))
+	require.Equal(t, Dot{X: 0x0, Y: 0x0}, HashToDot(0x0))
 }
