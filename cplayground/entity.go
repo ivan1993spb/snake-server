50a19d6b0ddd40edf98793962b88ad0c1a72cb70
Ivan Pushkin <iv.pushk@gmail.com>
2018-06-16 16:53:02 +0300

    Create entity in cplayground

---
+++ cplayground/entity.go
@@ -0,0 +1,31 @@
+package cplayground
+
+import (
+	"sync"
+
+	"github.com/ivan1993spb/snake-server/engine"
+)
+
+type entity struct {
+	object   interface{}
+	location engine.Location
+	mux      *sync.RWMutex
+}
+
+func (e *entity) GetLocation() engine.Location {
+	e.mux.RLock()
+	defer e.mux.RUnlock()
+	return e.location.Copy()
+}
+
+func (e *entity) GetObject() interface{} {
+	e.mux.RLock()
+	defer e.mux.RUnlock()
+	return e.object
+}
+
+func (e *entity) SetLocation(location engine.Location) {
+	e.mux.Lock()
+	defer e.mux.Unlock()
+	e.location = location.Copy()
+}
