0979284fa3737669439df54df2f97b3f60550cf4
Ivan Pushkin <pushkin13@bk.ru>
2018-07-08 14:59:53 +0300

    Create comments in object interfaces

--- objects/interfaces.go
+++ objects/interfaces.go
@@ -4,15 +4,21 @@ import "github.com/ivan1993spb/snake-server/engine"
 
 // Food interface describes methods that must be implemented all edible objects
 type Food interface {
+	// Bites object on dot dot and returns nutritional value nv, success flag -
+	// true if dot free and error err if error occurred
 	Bite(dot engine.Dot) (nv uint16, success bool, err error)
 }
 
 // Alive interface describes methods that must be implemented all alive objects
 type Alive interface {
+	// Hits object on dot dot with force force and returns success flag - true
+	// if dot free and error err if error occurred
 	Hit(dot engine.Dot, force float32) (success bool, err error)
 }
 
 // Object interface describes methods that must be implemented all not alive objects
 type Object interface {
+	// Breaks object on dot dot with force force, returns success flag true if dot
+	// free and error err if error occurred
 	Break(dot engine.Dot, force float32) (success bool, err error)
 }
