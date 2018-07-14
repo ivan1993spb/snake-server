a448e1c44e4f33293893c903e6a497a12d78eb8a
Ivan Pushkin <iv.pushk@gmail.com>
2018-07-15 00:36:44 +0300

    Create tests for playground.Playground

--- playground/playground_test.go
+++ playground/playground_test.go
@@ -10,26 +10,86 @@ import (
 	"github.com/ivan1993spb/snake-server/engine"
 )
 
+func Test_NewPlayground_CreatesPlayground(t *testing.T) {
+	tests := []struct {
+		width, height uint8
+	}{
+		{
+			width:  200,
+			height: 100,
+		},
+		{
+			width:  80,
+			height: 100,
+		},
+		{
+			width:  30,
+			height: 22,
+		},
+		{
+			width:  255,
+			height: 200,
+		},
+	}
+
+	for i, test := range tests {
+		pg, err := NewPlayground(test.width, test.height)
+		require.Nil(t, err, "test %d", i)
+		require.NotNil(t, pg, "test %d", i)
+		require.Equal(t, test.width, pg.area.Width(), "test %d", i)
+		require.Equal(t, test.height, pg.area.Height(), "test %d", i)
+		require.Equal(t, uint16(test.width)*uint16(test.height), pg.area.Size(), "test %d", i)
+	}
+}
+
 func Test_Playground_CreateObject(t *testing.T) {
-	object := &struct{}{}
+	object := &struct {
+		a int
+	}{
+		10,
+	}
 	location := engine.Location{engine.Dot{0, 0}}
 
-	pg, err := NewPlayground(100, 100)
-	require.Nil(t, err)
+	pg := &Playground{
+		cMap:       cmap.NewDefault(),
+		objects:    make([]interface{}, 0),
+		objectsMux: &sync.RWMutex{},
+		area:       engine.MustArea(100, 100),
+	}
 
-	err = pg.CreateObject(object, location)
+	err := pg.CreateObject(object, location)
 	require.Nil(t, err)
+	require.Len(t, pg.objects, 1)
+	require.Equal(t, object, pg.objects[0])
+	actual, ok := pg.cMap.Get(engine.Dot{0, 0}.Hash())
+	require.True(t, ok)
+	require.Equal(t, object, actual)
 }
 
 func Test_Playground_CreateObjectRandomRect(t *testing.T) {
-	object := &struct{}{}
+	object := &struct {
+		str string
+	}{
+		"ok",
+	}
 
-	pg, err := NewPlayground(100, 100)
-	require.Nil(t, err)
+	pg := &Playground{
+		cMap:       cmap.NewDefault(),
+		objects:    make([]interface{}, 0),
+		objectsMux: &sync.RWMutex{},
+		area:       engine.MustArea(100, 100),
+	}
 
 	location, err := pg.CreateObjectRandomRect(object, 10, 10)
 	require.Nil(t, err)
 	require.Len(t, location, 100)
+
+	for _, dot := range location {
+		require.True(t, pg.cMap.Has(dot.Hash()))
+		actual, ok := pg.cMap.Get(dot.Hash())
+		require.True(t, ok)
+		require.Equal(t, object, actual)
+	}
 }
 
 func Test_Playground_UpdateObject(t *testing.T) {
@@ -130,4 +190,207 @@ func Test_Playground_UpdateObjectAvailableDots_NotEmptyMap(t *testing.T) {
 	require.Equal(t, engine.Location{{1, 2}}, location)
 	require.Empty(t, pg.cMap.MGet(engine.Location{{1, 1}}.Hash()))
 	require.NotEmpty(t, pg.cMap.MGet(engine.Location{{1, 2}}.Hash()))
+}
+
+func Test_Playground_UpdateObjectAvailableDots_NotEmptyMap_BigObjects(t *testing.T) {
+	pg := &Playground{
+		cMap:       cmap.NewDefault(),
+		objects:    make([]interface{}, 0),
+		objectsMux: &sync.RWMutex{},
+		area:       engine.MustArea(100, 100),
+	}
+
+	object1 := &struct {
+		data string
+	}{"first"}
+	location1Old := engine.Location{
+		{1, 1},
+		{1, 2},
+		{1, 3},
+		{1, 4},
+		{1, 5},
+		{1, 6},
+	}
+	location1New := engine.Location{
+		{2, 0},
+		{2, 1},
+		{2, 2},
+		{1, 3},
+		{1, 4},
+		{1, 5},
+		{3, 6},
+		{3, 6},
+	}
+	location1Expected := engine.Location{
+		{2, 0},
+		{1, 3},
+		{1, 4},
+		{1, 5},
+		{3, 6},
+	}
+
+	pg.cMap.MSet(prepareMap(object1, location1Old))
+	pg.objects = append(pg.objects, object1)
+	for _, dot := range location1Old {
+		require.True(t, pg.cMap.Has(dot.Hash()))
+		actual, ok := pg.cMap.Get(dot.Hash())
+		require.True(t, ok)
+		require.Equal(t, object1, actual)
+	}
+
+	object2 := &struct {
+		data string
+	}{"second"}
+	location2 := engine.Location{
+		{2, 1},
+		{2, 2},
+		{2, 3},
+		{2, 4},
+		{2, 5},
+		{2, 6},
+	}
+
+	pg.cMap.MSet(prepareMap(object2, location2))
+	pg.objects = append(pg.objects, object2)
+	for _, dot := range location2 {
+		require.True(t, pg.cMap.Has(dot.Hash()))
+		actual, ok := pg.cMap.Get(dot.Hash())
+		require.True(t, ok)
+		require.Equal(t, object2, actual)
+	}
+
+	location1Actual, err := pg.UpdateObjectAvailableDots(object1, location1Old, location1New)
+	require.Nil(t, err)
+	require.True(t, location1Expected.Equals(location1Actual))
+
+	for _, dot := range pg.area.Dots() {
+		actualObject, ok := pg.cMap.Get(dot.Hash())
+
+		if location1Expected.Contains(dot) {
+			require.True(t, ok, "dot %s", dot)
+			require.Equal(t, object1, actualObject, "dot %s", dot)
+		} else if location2.Contains(dot) {
+			require.True(t, ok, "dot %s", dot)
+			require.Equal(t, object2, actualObject, "dot %s", dot)
+		} else {
+			require.False(t, ok, "dot %s", dot)
+			require.Nil(t, actualObject, "dot %s", dot)
+		}
+	}
+}
+
+func Test_Playground_CreateObjectRandomDot_SquareEmptyPlayground(t *testing.T) {
+	pg := &Playground{
+		cMap:       cmap.NewDefault(),
+		objects:    make([]interface{}, 0),
+		objectsMux: &sync.RWMutex{},
+		area:       engine.MustArea(100, 100),
+	}
+
+	object := &struct {
+		data string
+	}{"first"}
+
+	location, err := pg.CreateObjectRandomDot(object)
+	require.Nil(t, err)
+	require.Len(t, location, 1)
+
+	for _, dot := range pg.area.Dots() {
+		actualObject, ok := pg.cMap.Get(dot.Hash())
+
+		if location.Contains(dot) {
+			require.True(t, ok, "dot %s", dot)
+			require.Equal(t, object, actualObject, "dot %s", dot)
+		} else {
+			require.False(t, ok, "dot %s", dot)
+			require.Nil(t, actualObject, "dot %s", dot)
+		}
+	}
+}
+
+func Test_Playground_CreateObjectRandomDot_EmptyPlayground(t *testing.T) {
+	pg := &Playground{
+		cMap:       cmap.NewDefault(),
+		objects:    make([]interface{}, 0),
+		objectsMux: &sync.RWMutex{},
+		area:       engine.MustArea(200, 100),
+	}
+
+	object := &struct {
+		data string
+	}{"first"}
+
+	location, err := pg.CreateObjectRandomDot(object)
+	require.Nil(t, err)
+	require.Len(t, location, 1)
+
+	for _, dot := range pg.area.Dots() {
+		actualObject, ok := pg.cMap.Get(dot.Hash())
+
+		if location.Contains(dot) {
+			require.True(t, ok, "dot %s", dot)
+			require.Equal(t, object, actualObject, "dot %s", dot)
+		} else {
+			require.False(t, ok, "dot %s", dot)
+			require.Nil(t, actualObject, "dot %s", dot)
+		}
+	}
+}
+
+func Test_Playground_CreateObjectRandomRect_SquareEmptyPlayground_SquareRect(t *testing.T) {
+	pg := &Playground{
+		cMap:       cmap.NewDefault(),
+		objects:    make([]interface{}, 0),
+		objectsMux: &sync.RWMutex{},
+		area:       engine.MustArea(100, 100),
+	}
+
+	object := &struct {
+		data string
+	}{"first"}
+
+	location, err := pg.CreateObjectRandomRect(object, 10, 10)
+	require.Nil(t, err)
+	require.Len(t, location, 10*10)
+
+	for _, dot := range pg.area.Dots() {
+		actualObject, ok := pg.cMap.Get(dot.Hash())
+
+		if location.Contains(dot) {
+			require.True(t, ok, "dot %s", dot)
+			require.Equal(t, object, actualObject, "dot %s", dot)
+		} else {
+			require.False(t, ok, "dot %s", dot)
+			require.Nil(t, actualObject, "dot %s", dot)
+		}
+	}
+}
+
+func Test_Playground_CreateObjectRandomRect_SquareEmptyPlayground(t *testing.T) {
+	pg := &Playground{
+		cMap:       cmap.NewDefault(),
+		objects:    make([]interface{}, 0),
+		objectsMux: &sync.RWMutex{},
+		area:       engine.MustArea(100, 100),
+	}
+
+	object := &struct {
+		data string
+	}{"first"}
+
+	location, err := pg.CreateObjectRandomRect(object, 10, 8)
+	require.Nil(t, err)
+	require.Len(t, location, 10*8)
+
+	for _, dot := range pg.area.Dots() {
+		actualObject, ok := pg.cMap.Get(dot.Hash())
+
+		if location.Contains(dot) {
+			require.True(t, ok, "dot %s", dot)
+			require.Equal(t, object, actualObject, "dot %s", dot)
+		} else {
+			require.False(t, ok, "dot %s", dot)
+			require.Nil(t, actualObject, "dot %s", dot)
+		}
+	}
 }
