ef5bd1e1f73a93c7f04cf9385a04ab8ca0d556f1
Ivan Pushkin <pushkin13@bk.ru>
2019-03-30 22:08:23 +0300

    Refactor SnakeObserver

--- observers/snake_observer.go
+++ observers/snake_observer.go
@@ -23,23 +23,35 @@ func NewSnakeObserver(w world.Interface, logger logrus.FieldLogger) Observer {
 }
 
 func (so *SnakeObserver) Observe(stop <-chan struct{}) {
-	go func() {
-		for event := range so.world.Events(stop, chanSnakeObserverEventsBuffer) {
-			if event.Type == world.EventTypeObjectDelete {
-				if s, ok := event.Payload.(*snake.Snake); ok {
-					location := s.GetLocation().Copy()
-					if location.Empty() {
-						so.logger.Warn("snake dies and returns empty location")
-						continue
-					}
+	go so.run(stop)
+}
 
-					if c, err := corpse.NewCorpse(so.world, location); err != nil {
-						so.logger.WithError(err).Error("cannot create corpse")
-					} else {
-						c.Run(stop, so.logger)
-					}
-				}
-			}
+func (so *SnakeObserver) run(stop <-chan struct{}) {
+	so.listen(stop)
+}
+
+func (so *SnakeObserver) listen(stop <-chan struct{}) {
+	for event := range so.world.Events(stop, chanSnakeObserverEventsBuffer) {
+		so.handleEvent(event, stop)
+	}
+}
+
+func (so *SnakeObserver) handleEvent(event world.Event, stop <-chan struct{}) {
+	if event.Type != world.EventTypeObjectDelete {
+		return
+	}
+
+	if s, ok := event.Payload.(*snake.Snake); ok {
+		location := s.GetLocation().Copy()
+		if location.Empty() {
+			so.logger.Warn("snake dies and returns empty location")
+			return
 		}
-	}()
+
+		if c, err := corpse.NewCorpse(so.world, location); err != nil {
+			so.logger.WithError(err).Error("cannot create corpse")
+		} else {
+			c.Run(stop, so.logger)
+		}
+	}
 }
