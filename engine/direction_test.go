807c94891a3d9ff993dad74e34c23f81d140325e
Ivan Pushkin <pushkin13@bk.ru>
2019-08-10 11:27:10 +0300

    Create tests for engine.Direction

--- engine/direction_test.go
+++ engine/direction_test.go
@@ -29,3 +29,51 @@ func Test_Direction_CalculateDirection(t *testing.T) {
 		require.Equal(t, test.expectedDir, actualDir, fmt.Sprintf("number %d", i))
 	}
 }
+
+func Test_Direction_CalculateDirection_ReturnsValidDirectionForEqualDots(t *testing.T) {
+	require.True(t, ValidDirection(CalculateDirection(Dot{}, Dot{})))
+	require.True(t, ValidDirection(CalculateDirection(Dot{10, 10}, Dot{10, 10})))
+}
+
+func Test_ValidDirection_ValidatesDirectionsCorrectly(t *testing.T) {
+	tests := []struct {
+		direction   Direction
+		expectedRes bool
+	}{
+		{DirectionNorth, true},
+		{DirectionEast, true},
+		{DirectionSouth, true},
+		{DirectionWest, true},
+		{22, false},
+		{44, false},
+	}
+
+	for i, test := range tests {
+		require.Equal(t, test.expectedRes, ValidDirection(test.direction), fmt.Sprintf("number %d", i))
+	}
+}
+
+func Test_Direction_Reverse_ReversesDirection(t *testing.T) {
+	tests := []struct {
+		directionInput    Direction
+		directionExpected Direction
+		expectError       bool
+	}{
+		{DirectionNorth, DirectionSouth, false},
+		{DirectionEast, DirectionWest, false},
+		{DirectionSouth, DirectionNorth, false},
+		{DirectionWest, DirectionEast, false},
+		{22, 0, true},
+		{44, 0, true},
+	}
+
+	for i, test := range tests {
+		actualDirection, err := test.directionInput.Reverse()
+		if test.expectError {
+			require.NotNil(t, err, fmt.Sprintf("number %d not error", i))
+		} else {
+			require.Nil(t, err, fmt.Sprintf("number %d error", i))
+		}
+		require.Equal(t, test.directionExpected, actualDirection, fmt.Sprintf("number %d", i))
+	}
+}
