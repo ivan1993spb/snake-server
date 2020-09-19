3d093eac91aff1cb6a539b5af0083ef311a6f755
Ivan Pushkin <pushkin13@bk.ru>
2020-09-19 13:00:45 +0300

    issue #96 Add an explicit comment on engine's container

--- engine/container.go
+++ engine/container.go
@@ -3,7 +3,13 @@ package engine
 // Object is a game object
 type Object interface{}
 
-// Container contains an object placed on a map
+// Container contains an object placed on a map.
+//
+// As objects are addressed with unsafe's pointers, it is neccessary
+// to know the exact type of the stored object. However, as there are
+// a lot of different types of objects, you never know what you find
+// at a dot. Therefore, a container type is needed to wrap an unknown
+// game object with a certain type you can convert from a pointer.
 type Container struct {
 	object Object
 }
